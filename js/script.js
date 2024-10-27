79c1ff391493a6a3f2419f34fd9ae8f3c341129b
thatsaylary <woahaylary@gmail.com>
2024-10-27 21:17:05 +0800

    navbar animation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(SplitText);*/
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= 70) {
+        navbar.classList.add('navbar-scrolled')
+    } else if(window.scrollY < 70) {
+        navbar.classList.remove('navbar-scrolled')
+    }
+});
