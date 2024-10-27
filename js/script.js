28dba6969e16f75b9dee7e27bc7b8b28207f2c2c
thatsaylary <woahaylary@gmail.com>
2024-10-27 19:24:34 +0800

    js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText);*/
