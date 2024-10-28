1cfcdd5890c64c5e044793d666f0c1ecf375ea98
thatsaylary <woahaylary@gmail.com>
2024-10-28 13:30:38 +0800

    clean + js + scrollbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-/*import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(SplitText);*/
-
+// navbar scroll
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
@@ -13,3 +8,14 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('navbar-scrolled')
     }
 });
+
+// dynamic text
+(function (){
+    var words = ["Перевод", "Клин", "Тайп"]
+    i = 0;
+    setInterval(function(){
+        $('#words').fadeOut(function(){
+            $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
+        });
+    }, 2000)
+})()
